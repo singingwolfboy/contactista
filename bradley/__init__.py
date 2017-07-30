c60a71fefcc6a8a47ab7498d5385911d05b73fce
David Baumgold <david@davidbaumgold.com>
2017-07-30 11:25:21 -0400

    Depend on my fork of Flask-Security

--- bradley/__init__.py
+++ bradley/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from bradley.models import db, security, shell_context
 from bradley.models.auth import user_datastore
 from bradley.views.api import blueprint as api_bp
-from bradley.forms.security import LoginForm, ConfirmRegisterForm
 from bradley.admin import admin, admin_context
 from bradley.jwt import user_from_jwt_request
 
@@ -13,7 +12,7 @@ DEFAULT_CONFIG = {
     "SQLALCHEMY_DATABASE_URI": "sqlite:///db.sqlite3",
     "SQLALCHEMY_TRACK_MODIFICATIONS": False,
     "SECURITY_PASSWORD_HASH": "bcrypt",
-    "SECURITY_USER_IDENTITY_ATTRIBUTES": ("username",)
+    "SECURITY_USER_IDENTITY_ATTRIBUTES": ["username"],
 }
 
 
@@ -21,12 +20,7 @@ def create_app():
     app = Flask(__name__)
     app.config.update(DEFAULT_CONFIG)
     db.init_app(app)
-    state = security.init_app(
-        app,
-        datastore=user_datastore,
-        login_form=LoginForm,
-        confirm_register_form=ConfirmRegisterForm,
-    )
+    state = security.init_app(app, datastore=user_datastore)
     state.login_manager.request_loader(user_from_jwt_request)
     state.context_processor(admin_context)
     admin.init_app(app)
