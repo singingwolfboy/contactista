2e61b63bcffa7b7dcd73213d3e1de9cb745bdab3
David Baumgold <david@davidbaumgold.com>
2017-08-15 12:33:59 -0700

    Shorter pronouns fields

--- bradley/models/shared.py
+++ bradley/models/shared.py
@@ -3,23 +3,23 @@ from bradley.models import db
 
 class Pronouns(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    subject_pronoun = db.Column(db.String(50), nullable=False)
-    object_pronoun = db.Column(db.String(50), nullable=False)
+    subject = db.Column(db.String(50), nullable=False)
+    object = db.Column(db.String(50), nullable=False)
     possessive_determiner = db.Column(db.String(50), nullable=False)
-    possessive_pronoun = db.Column(db.String(50), nullable=False)
-    reflexive_pronoun = db.Column(db.String(50), nullable=False)
+    possessive = db.Column(db.String(50), nullable=False)
+    reflexive = db.Column(db.String(50), nullable=False)
 
     def __str__(self):
         return "{subject}/{object}".format(
-            subject=self.subject_pronoun,
-            object=self.object_pronoun,
+            subject=self.subject,
+            object=self.object,
         )
 
     def __repr__(self):
         return "<Pronouns {subj}/{obj}/{poss_det}/{poss}/{reflex}>".format(
-            subj=self.subject_pronoun,
-            obj=self.object_pronoun,
+            subj=self.subject,
+            obj=self.object,
             poss_det=self.possessive_determiner,
-            poss=self.possessive_pronoun,
-            reflex=self.reflexive_pronoun,
+            poss=self.possessive,
+            reflex=self.reflexive,
         )
