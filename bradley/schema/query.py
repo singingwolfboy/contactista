0eb91ee39b9ddcc4d214cde1b3e69be2a8a78454
David Baumgold <david@davidbaumgold.com>
2017-08-21 09:49:15 -0400

    user-scoped graphql fields

--- bradley/schema/query.py
+++ bradley/schema/query.py
@@ -1,15 +1,25 @@
 import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyConnectionField
+from graphene_sqlalchemy.utils import get_query
 from bradley.schema.types import Tag, User, Contact, Pronouns
 from flask_security import current_user
+
+
+class CurrentUserConnectionField(SQLAlchemyConnectionField):
+    @classmethod
+    def get_query(cls, model, context, info, args):
+        if current_user.is_anonymous:
+            return []
+        query = get_query(model, context)
+        return query.filter(user=current_user)
 
 
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
     me = graphene.Field(User)
-    tags = SQLAlchemyConnectionField(Tag)
-    contacts = SQLAlchemyConnectionField(Contact)
+    tags = CurrentUserConnectionField(Tag)
+    contacts = CurrentUserConnectionField(Contact)
     pronouns = SQLAlchemyConnectionField(Pronouns)
 
     def resolve_me(self, args, context, info):
--- tests/views/test_api.py
+++ tests/views/test_api.py
@@ -1,5 +1,6 @@
 import pytest
-from bradley.models import db, User
+from bradley.models import db, User, Contact
+from bradley.jwt import jwt_token_for_user
 
 
 def test_graphiql_enabled(client):
@@ -8,7 +9,7 @@ def test_graphiql_enabled(client):
 
 
 @pytest.mark.usefixtures("session")
-def test_unauthorized_contacts(client):
+def test_empty_contacts(client):
     query = """
     {
       contacts {
@@ -26,6 +27,69 @@ def test_unauthorized_contacts(client):
       "data": {
         "contacts": {
           "edges": []
+        }
+      }
+    }
+
+
+@pytest.mark.usefixtures("session")
+def test_unauthorized_contacts(client):
+    user = User(username="test")
+    contact = Contact(user=user, name="Sally")
+    db.session.add_all([user, contact])
+    db.session.commit()
+    query = """
+    {
+      contacts {
+        edges {
+          node {
+            id
+            name
+          }
+        }
+      }
+    }
+    """
+    resp = client.get("/graphql", data={"query": query})
+    assert resp.json == {
+      "data": {
+        "contacts": {
+          "edges": []
+        }
+      }
+    }
+
+
+@pytest.mark.usefixtures("session")
+def test_authorized_contacts(client):
+    user = User(username="test")
+    contact = Contact(user=user, name="Sally")
+    db.session.add_all([user, contact])
+    db.session.commit()
+    query = """
+    {
+      contacts {
+        edges {
+          node {
+            id
+            name
+          }
+        }
+      }
+    }
+    """
+    token = jwt_token_for_user(user)
+    headers = {"Authorization": "Bearer {token}".format(token=token.decode('utf-8'))}
+    resp = client.get("/graphql", data={"query": query}, headers=headers)
+    assert resp.json == {
+      "data": {
+        "contacts": {
+          "edges": [{
+            "node": {
+              "id": "Q29udGFjdDo0",
+              "name": "Sally",
+            }
+          }]
         }
       }
     }
